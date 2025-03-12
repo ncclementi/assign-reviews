a55004f58aad2442b4e61c925771195f41a2b6c2
Gil Forsyth <gil@forsyth.dev>
2025-03-12 17:17:10 -0400

    Restore previous SQL checks

--- notebooks/run-assignments.py
+++ notebooks/run-assignments.py
@@ -249,9 +249,7 @@ con.sql("table reviewer_assignments_02")
 
 # %%
 con.sql(
-    #"select count(*), string_agg(reviewer_id), len(assigned_submission_ids) as num_submissions from reviewer_assignments_02 group by num_submissions"  # noqa: E501
-        "select count(*), len(assigned_submission_ids) as num_submissions from reviewer_assignments_02 group by num_submissions"  # noqa: E501
-
+    "select count(*), string_agg(reviewer_id), len(assigned_submission_ids) as num_submissions from reviewer_assignments_02 group by num_submissions"  # noqa: E501
 )
 
 # %%
@@ -278,8 +276,7 @@ con.sql("table submission_assignments_02")
 
 # %%
 con.sql(
-#select string_agg(submission_id), count(track), len(assigned_reviewer_ids) from submission_assignments_02 group by len(assigned_reviewer_ids)
-"select track, len(assigned_reviewer_ids) from submission_assignments_02 group by track, len(assigned_reviewer_ids) order by 2 DESC"
+    "select string_agg(submission_id), count(track), len(assigned_reviewer_ids) from submission_assignments_02 group by len(assigned_reviewer_ids)"
 )
 
 # %% [markdown]
